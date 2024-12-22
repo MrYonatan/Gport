a9a2b890edfaa99fac84c2d50a966199fefe58c8
MrYonatan <yonatanassefa60@gmail.com>
2024-12-22 23:21:03 +0300

    deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
